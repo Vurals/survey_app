2769d0a3a95d05f4e9590370dde77de3fc00110b
boracun <cun2424@gmail.com>
2022-03-30 00:45:06 +0300

    Delete unnecessary comment

--- Appium/src/test/java/ProjectTests.java
+++ Appium/src/test/java/ProjectTests.java
@@ -464,7 +464,6 @@ public class ProjectTests {
         Thread.sleep(1000);
         dayField.sendKeys("24");
         Thread.sleep(1000);
-        //webDriverWait.until(ExpectedConditions.textToBePresentInElement(dayField, "24"));
         appiumDriver.hideKeyboard();
         monthField.click();
         Thread.sleep(1000);
